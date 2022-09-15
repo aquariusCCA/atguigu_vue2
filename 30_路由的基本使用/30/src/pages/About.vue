fde6300c1da43c74d76e0fdfa86261213b9d929f
Kevin <lin860210@i-mps.com>
2022-09-15 14:16:42 +0800

    commit  30_路由的基本使用

---
+++ 30_路由的基本使用/30/src/pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+	<h2>我是About的内容</h2>
+</template>
+
+<script>
+	export default {
+		name: 'About',
+		beforeDestroy() {
+			console.log('About 組件即將被銷毀了')
+		},
+		mounted() {
+			console.log('About 組件掛載完畢了')
+		},
+	}
+</script>
---
+++ 30_路由的基本使用/30/src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+	<h2>我是Home的内容</h2>
+</template>
+
+<script>
+	export default {
+		name:'Home',
+		beforeDestroy() {
+			console.log('Home 組件即將被銷毀了')
+		},
+		mounted() {
+			console.log('Home 組件掛載完畢了')
+		},
+	}
+</script>
